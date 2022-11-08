af95c6fa23a28652fca7dc12086f2931080f92fe
Harsh Mehta <harsh.mehta@cleantechsolar.com>
2022-11-09 02:41:33 +0530

    added validation to transfer component

--- src/app/admin/transfer/transfer.component.ts
+++ src/app/admin/transfer/transfer.component.ts
@@ -13,12 +13,10 @@ export class TransferComponent implements OnInit {
 
   users: Iuser[] = this.userService.listUsers();
   transfer!: FormGroup;
-  Fbal_btn = false;
-  Tbal_btn = false;
   from_bal: number = 0;
   to_bal: number = 0;
 
-  constructor(public userService: UserService, private fb: FormBuilder,private router: Router) { }
+  constructor(public userService: UserService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.users = this.userService.listUsers();
@@ -34,29 +32,15 @@ export class TransferComponent implements OnInit {
     return this.transfer.controls;
   }
 
-  retBalance(email: string, type: string) {
-    console.log("email rec:", email);
-    if (type == 'from') {
-      this.Fbal_btn = true;
-      this.from_bal = this.userService.checkBalance(email);
-      console.log(this.from_bal);
-    }
-    else {
-      this.Tbal_btn = true;
-      this.to_bal = this.userService.checkBalance(email);
-      console.log(this.to_bal);
-    }
+  accountChange(acc: string, type: string) {
+    if (type == 'from')
+      this.from_bal = this.userService.checkBalance(acc)
+    else
+      this.to_bal = this.userService.checkBalance(acc)
   }
 
-  resetForm() {
-    this.Fbal_btn = false;
-    this.Tbal_btn = false;
-    this.from_bal = 0;
-    this.to_bal = 0;
-  }
-
-  onSubmit() { 
-    var msg=this.userService.transferAmount(this.transfer.value.from,this.transfer.value.to,this.transfer.value.amount,);
+  onSubmit() {
+    var msg = this.userService.transferAmount(this.transfer.value.from, this.transfer.value.to, this.transfer.value.amount,);
     window.alert(msg);
     console.log(msg);
     this.router.navigate(['/admin/home']);
